0a0e69540589185fe253f3856a3ba6df899b8afb
joshmclynn <mclynn_josh@hotmail.co.uk>
2022-07-28 20:54:11 +0100

    adding aws bucket

--- gardensdirect/settings.py
+++ gardensdirect/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.isfile("env.py"):
     import env
@@ -118,12 +119,7 @@ WSGI_APPLICATION = 'gardensdirect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
