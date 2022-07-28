148601aafd6365629c095ed7436ae599e63637f6
joshmclynn <mclynn_josh@hotmail.co.uk>
2022-07-28 21:53:31 +0100

    troubleshooting

--- gardensdirect/settings.py
+++ gardensdirect/settings.py
@@ -130,7 +130,7 @@ if 'DATABASE_URL' in os.environ:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
